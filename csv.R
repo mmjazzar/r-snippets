0b36cbfc645d9667eb3a07b347e1d5d0cf2bfc4f
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-08-23 13:12:01 +0200

    Update csv.R

--- csv.R
+++ csv.R
@@ -9,3 +9,10 @@ write.csv(z, file = "x.csv")
 #appending values
 # k is a dataframe
 if (file.exists(paste('code', 'test.csv'))==FALSE) write.csv(k, file=paste('code', 'test.csv'), row.names=F)
+
+
+# better method
+
+  write.table(k, "export.csv", row.names=F, na="NA", 
+              append = T, quote= FALSE, sep = ",", col.names = F)
+  
