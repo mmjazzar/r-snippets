15555b587cd76c39b643bec5c4df5aed5b419ded
Maged M. Eljazzar <eng.maged91@hotmail.com>
2017-08-23 12:50:27 +0200

    Update csv.R
    appending values

--- csv.R
+++ csv.R
@@ -4,3 +4,8 @@ mydata <- read.csv("data science - sample date.csv", header = TRUE, na.strings=c
 #export CSV
 z <- count(mydata = mydata$cityname)
 write.csv(z, file = "x.csv")
+
+
+#appending values
+# k is a dataframe
+if (file.exists(paste('code', 'test.csv'))==FALSE) write.csv(k, file=paste('code', 'test.csv'), row.names=F)
